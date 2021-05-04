c752844f9bb33360710ab11c22d3b8828965395a
EC2 Default User <ec2-user@ip-172-31-44-142.ec2.internal>
2021-05-04 08:39:13 +0000

    [finish]2-7

---
+++ ruby/lesson7-2.rb
@@ -0,0 +1,14 @@
+puts "計算を始めます。"
+puts "何回繰り返しますか？"
+a = gets.to_i
+i = 1
+b = gets.to_i
+c = gets.to_i
+while i <= a do 
+  puts "2つの値を入力してください"
+  puts b + c
+  puts b - c
+  puts b * c
+  puts b / c
+  i += 1
+end
